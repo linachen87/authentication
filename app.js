e3da206b9ff5e574f8600f3cd2ef123e703d2fc4
Howie Chi <lina10895044@gmail.com>
2022-06-17 14:59:15 -0700

    level2  use env and encrypt secret

---
+++ app.js
@@ -0,0 +1,72 @@
+//jshint esversion:6
+require("dotenv").config();
+const express = require("express");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
+const app = express();
+app.set("view engine","ejs");
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/userDB");
+const userSchema = new mongoose.Schema({
+  email:String,
+  password:String
+});
+const secret = "this is the secret encrypt";
+userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:["password"]});
+const User = mongoose.model("User", userSchema);
+
+app.get("/",(req, res)=>{
+  res.render("home");
+});
+
+app.get("/register",(req, res)=>{
+  res.render("register");
+});
+
+app.get("/login",(req, res)=>{
+  res.render("login");
+});
+
+app.post("/register",(req, res)=>{
+  const newUser = new User({
+    email:req.body.username,
+    password:req.body.password
+  });
+  newUser.save((err)=>{
+    if(err){
+      console.log(err);
+    }else{
+      console.log("successfully added!")
+      res.render("secrets");
+    }
+  });
+});
+
+app.post("/login",(req, res)=>{
+  const email = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({email:email},(err, foundResult)=>{
+    if(!err){
+      if(foundResult){
+        if(foundResult.password === password){
+          res.render("secrets");
+        }else{
+          res.send("wrong password!");
+        }
+      }
+      else{
+        res.send("No user match!");
+      }
+    }
+  });
+});
+
+app.listen(3000, ()=>{
+  console.log("Started listening on port 3000");
+});
